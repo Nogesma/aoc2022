b2b728161cdd0b7068c4ab4480e9a6d93e2a3f1f
Mano Ségransan <mano.segransan@protonmail.com>
2022-12-03 14:01:25 +0100

    d3p2

--- src/main.rs
+++ src/main.rs
@@ -10,33 +10,55 @@ fn get_input() -> Enumerate<Lines<BufReader<File>>> {
     let path = Path::new("input");
     let display = path.display();
 
-    let file = match File::open(&path) {
+    let file = match File::open(path) {
         Err(why) => panic!("couldn't open {}: {}", display, why),
         Ok(file) => file,
     };
 
     let reader = BufReader::new(file);
-    return reader.lines().enumerate();
+    reader.lines().enumerate()
 }
 
-fn get_common_item((_, line): (usize, Result<String, std::io::Error>)) -> i32 {
-    let line = line.unwrap();
+fn get_common_item(
+    e1: Result<String, std::io::Error>,
+    e2: Result<String, std::io::Error>,
+    e3: Result<String, std::io::Error>,
+) -> i32 {
+    let e1 = e1.unwrap();
+    let e2 = e2.unwrap();
+    let e3 = e3.unwrap();
 
-    let (first, second) = line.split_at(line.len() / 2);
-
-    for c in first.chars() {
-        if second.contains(c) {
+    for c in e1.chars() {
+        if e2.contains(c) && e3.contains(c) {
             if c.is_uppercase() {
                 return c as i32 - 'A' as i32 + 27;
             }
             return c as i32 - 'a' as i32 + 1;
         }
     }
-    panic!("No common chars: {} {}, {}", first, second, line);
+    panic!("No common chars:\n{}\n{}\n{}", e1, e2, e3);
 }
 
 fn get_total_score(input: &mut Enumerate<Lines<BufReader<File>>>) -> i32 {
-    return input.map(get_common_item).sum();
+    let mut total = 0;
+
+    loop {
+        let e1 = input.next();
+        if let None = e1 {
+            return total;
+        }
+        let e2 = input.next();
+        let e3 = input.next();
+
+        if let None = e2 {
+            panic!("Missing second elf");
+        }
+        if let None = e3 {
+            panic!("Missing third elf");
+        }
+
+        total += get_common_item(e1.unwrap().1, e2.unwrap().1, e3.unwrap().1);
+    }
 }
 
 fn main() {
